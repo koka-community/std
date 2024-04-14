b5b1a69c6a02239482a893cb92af6e05a3cc239b
ki11errabbit <ki11errabbit@posteo.net>
2024-04-14 12:08:18 -0600

    feat: changed resize to use unsafe-idx

--- std/inline/core-extras.h
+++ std/inline/core-extras.h
@@ -18,3 +18,11 @@ static inline kk_unit_t kk_vector_unsafe_assign_community( kk_vector_t v, kk_ssi
   kk_vector_drop(v,ctx); // TODO: use borrowing
   return kk_Unit;
 }
+
+// This is taken from here https://github.com/koka-lang/koka/blob/dev/kklib/include/kklib/vector.h
+// Only the name is modified so that it doesn't collide into the original.
+static inline kk_box_t kk_vector_at_borrow_community(const kk_vector_t v, kk_ssize_t i, kk_context_t* ctx) {
+  kk_assert(i < kk_vector_len_borrow(v,ctx));
+  kk_box_t res = kk_box_dup(kk_vector_buf_borrow(v, NULL, ctx)[i],ctx);
+  return res;
+}
