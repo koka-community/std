dd11a47fb68a3c0b97adbfdc06cb896e6606569e
ki11errabbit <ki11errabbit@posteo.net>
2024-04-16 17:10:51 -0600

    fix: added header guards

--- std/inline/core-extras.h
+++ std/inline/core-extras.h
@@ -1,2 +1,35 @@
+#ifndef KK_CORE_EXTRAS_H
+#define KK_CORE_EXTRAS_H
+
 
 kk_unit_t kk_vector_clear(kk_vector_t v, kk_ssize_t stop, kk_context_t* ctx);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /* KK_CORE_EXTRAS_H */
