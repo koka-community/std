502f76042ee079662ff3247816881e7ff0cdc993
ki11errabbit <ki11errabbit@posteo.net>
2024-04-16 09:43:43 -0600

    feat: added way to clear out vlist

---
+++ std/inline/core-extras.c
@@ -0,0 +1,13 @@
+
+
+
+kk_unit_t kk_vector_clear(kk_vector_t v, kk_ssize_t stop, kk_context_t* ctx) {
+    kk_ssize_t length;
+    kk_box_t* vec = kk_vector_buf_borrow(v, &length, ctx);
+    const kk_ssize_t true_stop = (stop > length ? length : stop);
+    for (kk_ssize_t i = 0; i < true_stop; i++) {
+        kk_box_drop(vec[i], ctx);
+        vec[i] = kk_box_null();
+    }
+    return kk_Unit;
+}
---
+++ std/inline/core-extras.h
@@ -0,0 +1,2 @@
+
+kk_unit_t kk_vector_clear(kk_vector_t v, kk_ssize_t stop, kk_context_t* ctx);
