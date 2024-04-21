27bd8fefe4c1b8bb58188221cc1bf9e2419d3ed1
ki11errabbit <ki11errabbit@posteo.net>
2024-04-20 20:45:12 -0600

    feat: added deque via circular vector, added a way to clear vectors at an index

--- std/inline/core-extras.c
+++ std/inline/core-extras.c
@@ -11,3 +11,11 @@ kk_unit_t kk_vector_clear(kk_vector_t v, kk_ssize_t stop, kk_context_t* ctx) {
     }
     return kk_Unit;
 }
+
+kk_unit_t kk_vector_clear_at(kk_vector_t v, kk_ssize_t pos, kk_context_t* ctx) {
+    kk_ssize_t length;
+    kk_box_t* vec = kk_vector_buf_borrow(v, &length, ctx);
+    kk_box_drop(vec[pos], ctx);
+    vec[pos] = kk_box_null();
+    return kk_Unit;
+}
