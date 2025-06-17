0f87b6c2949b6f94a1300a24a7990fc1e216aca2
amisto <amisto@qq.com>
2025-06-18 00:29:43 +0800

    use kk_ssize_t instead of ssize_t for consistency

--- std/inline/core-extras.h
+++ std/inline/core-extras.h
@@ -3,6 +3,6 @@ kk_unit_t kk_vector_clear(kk_vector_t v, kk_ssize_t stop, kk_context_t* ctx);
 
 kk_unit_t kk_vector_clear_at(kk_vector_t v, kk_ssize_t pos, kk_context_t* ctx);
 
-kk_vector_t kk_vector_from_cint32array(int32_t* array, ssize_t len, kk_context_t* ctx); 
-kk_vector_t kk_vector_from_cint64array(int64_t* array, ssize_t len, kk_context_t* ctx); 
-kk_vector_t kk_vector_from_cintarray(kk_intx_t* carray, ssize_t len, kk_context_t* ctx);
+kk_vector_t kk_vector_from_cint32array(int32_t* array, kk_ssize_t len, kk_context_t* ctx); 
+kk_vector_t kk_vector_from_cint64array(int64_t* array, kk_ssize_t len, kk_context_t* ctx); 
+kk_vector_t kk_vector_from_cintarray(kk_intx_t* carray, kk_ssize_t len, kk_context_t* ctx);
